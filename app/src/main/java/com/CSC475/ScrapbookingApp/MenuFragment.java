7107f99f0ee61bfe61b216e68be661a1d616ef70
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2023-01-26 04:45:18 -0500

    started working on the layout for android application that is supposed to show a collage of landscape images. TODO: get fragment navigation to work

---
+++ app/src/main/java/com/CSC475/ScrapbookingApp/MenuFragment.java
@@ -0,0 +1,47 @@
+package com.CSC475.ScrapbookingApp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.CSC475.ScrapbookingApp.databinding.FragmentMenuBinding;
+
+public class MenuFragment extends Fragment {
+
+    private FragmentMenuBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentMenuBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.menuButtonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(MenuFragment.this)
+                        .navigate(R.id.action_MenuFragment_to_CollageFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
